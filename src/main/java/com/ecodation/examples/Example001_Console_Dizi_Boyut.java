f73fba6706b68b38b974e6afe7ca4a1b17b06541
Hamit Mızrak <hamitmizrak@gmail.com>
2020-10-02 16:04:25 +0300

    Örnekler-1

---
+++ src/main/java/com/ecodation/examples/Example001_Console_Dizi_Boyut.java
@@ -0,0 +1,31 @@
+package com.ecodation.examples;
+
+import java.util.Scanner;
+
+public class Example001_Console_Dizi_Boyut {
+
+	public static void main(String[] args) {
+		Scanner klavye = new Scanner(System.in);
+		int satir, sutun;
+		String sembol;
+		System.out.println("lütfen sembol giriniz");
+		sembol = klavye.nextLine();
+
+		System.out.println("lütfen satır için sayı giriniz.");
+		satir = klavye.nextInt();
+
+		System.out.println("lütfen sutun için sayı giriniz.");
+		sutun = klavye.nextInt();
+		System.out.println(" girdiğiniz satır:" + satir + " girdiğiniz sutun: " + sutun);
+
+		int[][] matrix = new int[satir][sutun];
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				System.out.print(matrix[i][j] = Integer.valueOf(sembol));
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/main/java/com/ecodation/examples/Example007_HesapMakinesi.java
@@ -0,0 +1,55 @@
+package com.ecodation.examples;
+
+import java.util.Scanner;
+
+public class Example007_HesapMakinesi {
+	public static void main(String[] args) {
+		hesapMakinesi();
+	}
+
+	public static void hesapMakinesi() {
+		Scanner klavye = new Scanner(System.in);
+		int sayi2, sayi3;
+		System.out.println("1.sayıyı giriniz");
+		sayi2 = klavye.nextInt();
+		System.out.println("2.sayıyı giriniz.");
+		sayi3 = klavye.nextInt();
+
+		int top = topla(sayi2, sayi3);
+		System.out.println("Toplamları: " + top);
+
+		int cik = cikar(sayi2, sayi3);
+		System.out.println("cikar: " + cik);
+
+		int carp = carp(sayi2, sayi3);
+		System.out.println("carpımları: " + carp);
+
+		int bol = bolme(sayi2, sayi3);
+		System.out.println("Bölüm: " + bol);
+
+		int kalan = kalan(sayi2, sayi3);
+		System.out.println("Kalanları: " + kalan);
+
+	}
+
+	public static int topla(int sayi, int sayi2) {
+		return (sayi + sayi2);
+	}
+
+	public static int cikar(int sayi, int sayi2) {
+		return (sayi - sayi2);
+	}
+
+	public static int carp(int sayi, int sayi2) {
+		return (sayi * sayi2);
+	}
+
+	public static int bolme(int sayi, int sayi2) {
+		return (sayi / sayi2);
+	}
+
+	public static int kalan(int sayi, int sayi2) {
+		return (sayi % sayi2);
+	}
+
+}
